19b880e227e7dfe104cf4ceb8f1c21a873b8c500
David Nasibu <adjinasibusaidi@gmail.com>
2026-05-09 22:09:06 +0200

    Final update for W02 ASP.NET Core assignment

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1478522180fc644e3fa19cdb76e9040af73488")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cac965f7c9bee5be74b34248d2615a84bd9eb74c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
